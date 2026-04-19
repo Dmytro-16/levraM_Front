47979210ef2aa351239a600044cfa1d1cc8223f2
Dmytro-16 <dmytro.semenchuk@outlook.com>
2026-04-19 15:12:13 +0200

    mis a jour v2 responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,9 +34,9 @@ const Header = ({ search, setSearch, token, username, handleToken }) => {
             <div className="Button_R">
               <div className="SearchBtn">
                 <i
-                  className="fa-solid fa-magnifying-glass"
-                  aria-hidden="true"
-                ></i>
+                  // className="fa-solid fa-magnifying-glass"
+                  // aria-hidden="false"
+                > <svg className="SearchIcon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></svg></i>
 
                 <input
                   className="SearchBar"
